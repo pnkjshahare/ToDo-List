38d63b705b7e74f07ecffd8c6c59120a915c966a
pnkjshahare <pankajsahahre.04@gmail.com>
2024-12-17 23:26:57 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import react from 'react'
 import './App.css'
+import Todo from './component/Todo';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='bg-stone-900 grid py-4 min-h-screen'>
+      <Todo/>
+    </div>
   )
 }
 
-export default App
+export default App;
